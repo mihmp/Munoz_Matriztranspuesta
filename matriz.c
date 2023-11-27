87e18639ac5083173b2777ac1ec83456efc54c76
Pablo <pablo.munoz.proano@udla.edu.ec>
2023-11-27 16:35:57 -0500

    Se mejora la redaccion del codigo

--- matriz.c
+++ matriz.c
@@ -2,56 +2,57 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define FILAS_MAX 10
-#define COLUMNAS_MAX 10
+#define FILAS_MAXIMAS 10
+#define COLUMNAS_MAXIMAS 10
 
-void llenarMatriz(int matriz[FILAS_MAX][COLUMNAS_MAX], int filas, int columnas) {
+void llenarMatriz(int matriz[FILAS_MAXIMAS][COLUMNAS_MAXIMAS], int num_filas, int num_columnas) {
     srand(time(NULL));
     
-    for (int i = 0; i < filas; i++) {
-        for (int j = 0; j < columnas; j++) {
-            matriz[i][j] = rand() % 101; // Generar números aleatorios entre 0 y 100
+    for (int i = 0; i < num_filas; i++) {
+        for (int j = 0; j < num_columnas; j++) {
+            matriz[i][j] = rand() % 101; 
         }
     }
 }
 
-void imprimirMatriz(int matriz[FILAS_MAX][COLUMNAS_MAX], int filas, int columnas) {
-    for (int i = 0; i < filas; i++) {
-        for (int j = 0; j < columnas; j++) {
+void imprimirMatriz(int matriz[FILAS_MAXIMAS][COLUMNAS_MAXIMAS], int num_filas, int num_columnas) {
+    for (int i = 0; i < num_filas; i++) {
+        for (int j = 0; j < num_columnas; j++) {
             printf("%d ", matriz[i][j]);
         }
         printf("\n");
     }
 }
 
-void calcularTranspuesta(int matriz[FILAS_MAX][COLUMNAS_MAX], int transpuesta[COLUMNAS_MAX][FILAS_MAX], int filas, int columnas) {
-    for (int i = 0; i < filas; i++) {
-        for (int j = 0; j < columnas; j++) {
+void calcularTranspuesta(int matriz[FILAS_MAXIMAS][COLUMNAS_MAXIMAS], int transpuesta[COLUMNAS_MAXIMAS][FILAS_MAXIMAS], int num_filas, int num_columnas) {
+    for (int i = 0; i < num_filas; i++) {
+        for (int j = 0; j < num_columnas; j++) {
             transpuesta[j][i] = matriz[i][j];
         }
     }
 }
 
 int main() {
-    int filas, columnas;
-    int matriz[FILAS_MAX][COLUMNAS_MAX];
-    int transpuesta[COLUMNAS_MAX][FILAS_MAX];
+    int num_filas, num_columnas;
+    int matriz[FILAS_MAXIMAS][COLUMNAS_MAXIMAS];
+    int transpuesta[COLUMNAS_MAXIMAS][FILAS_MAXIMAS];
     
     printf("Ingrese el número de filas de la matriz: ");
-    scanf("%d", &filas);
+    scanf("%d", &num_filas);
     printf("Ingrese el número de columnas de la matriz: ");
-    scanf("%d", &columnas);
+    scanf("%d", &num_columnas);
     
-    llenarMatriz(matriz, filas, columnas);
+    llenarMatriz(matriz, num_filas, num_columnas);
     
     printf("\nMatriz original:\n");
-    imprimirMatriz(matriz, filas, columnas);
+    imprimirMatriz(matriz, num_filas, num_columnas);
     
-    calcularTranspuesta(matriz, transpuesta, filas, columnas);
+    calcularTranspuesta(matriz, transpuesta, num_filas, num_columnas);
     
     printf("\nMatriz transpuesta:\n");
-    imprimirMatriz(transpuesta, columnas, filas);
+    imprimirMatriz(transpuesta, num_columnas, num_filas);
     
     return 0;
 }
+
 
